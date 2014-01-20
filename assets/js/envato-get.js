08892228a5525c2c479341779379ad21f630b844
cosminlupu <cosmin.lupu90@gmail.com>
2014-01-20 20:23:13 +0100

    Working prototype of getting the data via the JSON and displaying it.

--- assets/js/envato-get.js
+++ assets/js/envato-get.js
@@ -6,18 +6,48 @@ $( document ).ready(function() {
     var seller;
     var apiKey;
     var purchaseID;
+    var envURL;
     
-    $('#jsonForm').ajaxForm({ 
-        // dataType identifies the expected content type of the server response 
-        dataType:  'json', 
- 
-        // success identifies the function to invoke when the server response 
-        // has been received 
-        success:   processJson 
-    }); 
+    
+    //User Info Variables
+    
+    var buyerUsername;
+    var buyDate;
+    var itemID;
+    var itemName;
+    var license
+    
+    
+        // bind form using ajaxForm 
+        $('#jsonForm').ajaxForm({ 
+            // dataType identifies the expected content type of the server response 
+            dataType:  'json', 
+     
+            // success identifies the function to invoke when the server response 
+            // has been received 
+            success:   processJson 
+        }); 
     
     function processJson(data) { 
-    // 'data' is the json object returned from the server 
-    alert(data.message); 
-}
+        // 'data' is the json object returned from the server 
+        apiKey = data.envForm.apiKey;
+        seller = data.envForm.sellerUsername;
+        purchaseID = data.envForm.purchaseCode;
+        
+        envURL = urlEnv+seller+"/"+apiKey+"/verify-purchase:"+purchaseID+".json";
+        
+        $.getJSON(envURL, function(purchaseData) {
+            $(".infoBox").css("display", "block");
+            $(".itemName").html(purchaseData["verify-purchase"].item_name);
+            $(".itemID").html(purchaseData["verify-purchase"].item_id);
+            $(".buyerUsername").html(purchaseData["verify-purchase"].buyer);
+            $(".purchaseDate").html(purchaseData["verify-purchase"].created_at);
+            $(".licenseType").html(purchaseData["verify-purchase"].licence);
+        });
+    }
+    
+    
+
+    
+    
 });
---
+++ page.php
@@ -0,0 +1,7 @@
+<?php  echo '{
+"envForm": { "sellerUsername":"' . $_POST['seller-username'] . '",
+  "apiKey":"' . $_POST['api-key'] . '", 
+  "purchaseCode":"' . $_POST['purchase-code'] . '",
+  "spamNumber":"' . $_POST['spam-number'] . '"
+}  }';  ?>
+
